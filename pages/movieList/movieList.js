204534785e04e15a3e47928f4a34fc24fb5ad692
李杰 <lijie-02@newrank.cn>
2018-05-03 17:25:21 +0800

    update

--- pages/movieList/movieList.js
+++ pages/movieList/movieList.js
@@ -1,11 +1,23 @@
 // pages/movieList/movieList.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    swiper: {
+      autoplay: true,
+      interval: 2000,
+      duration:   200
+    },
+    typeList: [
+      {key: 'in_theaters', count: 15}, // 正则热映
+      {key: 'coming_soon', count: 10}, // 即将上映
+      {key: 'new_movies', count: 20}, // 新片榜
+      {key: 'us_box', count: 8}, // 北美票房榜
+      {key: 'top250',count: 15} // top250
+    ]
   },
 
   /**
@@ -15,6 +27,20 @@ Page({
     wx.setNavigationBarTitle({
       title: '电影榜单'
     })
+    // wx.showLoading({
+    //   title: '拼命加载中',
+    //   mask: true
+    // })
+    // 获取多个类型的数据
+    // const requestArr = typeList.map((item) => {
+    //   return app.douban.find(item.key, 0, item.count)
+    //     .then(res => {
+    //       item.subjects = res.subjects
+    //     })
+    // })
+    // Promise.all(requestArr).then(() => {
+    //   wx.hideLoding()
+    // })
   },
 
   /**
